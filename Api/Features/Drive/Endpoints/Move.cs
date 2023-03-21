b6c2f8afe8d4fe496dfea0de4608da7e24466412
TheBinaryGuy <hello@thebinary.dev>
2023-03-21 22:18:09 +0530

    added support for previous version when moving files

--- Api/Features/Drive/Endpoints/Move.cs
+++ Api/Features/Drive/Endpoints/Move.cs
@@ -23,13 +23,15 @@ public static class Move
     private static async Task<Results<Ok, ValidationProblem>> MoveFolderHandler(
         MoveRequest req, WorkspaceDbContext ctx, CancellationToken ct)
     {
-        var targetFolder = await ctx.Folders.FirstOrDefaultAsync(f => f.Id == req.TargetFolderId, ct);
+        var targetFolder =
+            await ctx.Folders.FirstOrDefaultAsync(f => f.Id == req.TargetFolderId, ct);
         if (targetFolder is null)
         {
             throw new AppException("Invalid target folder.");
         }
 
-        var folders = ctx.Folders.Where(f => req.Folders.Select(reqFolder => reqFolder.Id).Contains(f.Id));
+        var folders =
+            ctx.Folders.Where(f => req.Folders.Select(reqFolder => reqFolder.Id).Contains(f.Id));
         foreach (var folder in folders)
         {
             if (folder.Id == targetFolder.Id)
@@ -45,14 +47,27 @@ public static class Move
             folder.MaterializedPath = $"{targetFolder.MaterializedPath}{folder.Id}/";
         }
 
-        var files = ctx.Files.Where(f => req.Files.Select(reqFile => reqFile.Id).Contains(f.Id));
+        var files = await ctx.Files.Where(f =>
+            req.Files.Select(reqFile => reqFile.Id).Contains(f.Id)
+        ).ToListAsync(ct);
+        var previousVersions = await ctx.Files.Where(f =>
+            req.Files.Select(reqFile => reqFile.PreviousVersionId).Contains(f.Id)
+        ).ToListAsync(ct);
+
         foreach (var file in files)
         {
-            var reqFolder = req.Files.First(r => r.Id == file.Id);
+            var reqFile = req.Files.First(r => r.Id == file.Id);
+            var previousVersion =
+                previousVersions.FirstOrDefault(f => f.Id == reqFile.PreviousVersionId);
+            if (previousVersion is not null)
+            {
+                previousVersion.FileStatus = FileStatus.OldVersion;
+                file.PreviousVersionId = previousVersion.Id;
+            }
 
             file.ParentFolderId = targetFolder.Id;
-            file.MetadataBundle = reqFolder.MetadataBundle;
-            file.KeyBundle = reqFolder.KeyBundle;
+            file.MetadataBundle = reqFile.MetadataBundle;
+            file.KeyBundle = reqFile.KeyBundle;
             file.MaterializedPath = $"{targetFolder.MaterializedPath}{file.Id}";
         }
 
@@ -64,7 +79,11 @@ public static class Move
 
 public sealed record NodeToMove(Guid Id, MetadataBundle MetadataBundle, SecretKeyBundle KeyBundle);
 
-public sealed record MoveRequest(Guid TargetFolderId, NodeToMove[] Folders, NodeToMove[] Files);
+public sealed record FileNodeToMove(Guid Id, MetadataBundle MetadataBundle,
+    SecretKeyBundle KeyBundle,
+    Guid? PreviousVersionId);
+
+public sealed record MoveRequest(Guid TargetFolderId, NodeToMove[] Folders, FileNodeToMove[] Files);
 
 public sealed class MoveRequestValidator : AbstractValidator<MoveRequest>
 {
